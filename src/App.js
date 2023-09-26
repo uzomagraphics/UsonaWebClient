b322331418341cc5a74551a90dc17bd9a8f92798
diego amozoo <uzoma@diegos-MacBook-Pro-2.local>
2023-09-26 13:30:04 -0600

    firs frame full functionality

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ import './App.css';
 // import required modules
 import { Pagination } from "swiper/modules";
 
+//FONTS
+import "./fonts/RooneySans-Bold.ttf";
+import "./fonts/RooneySans-Regular.ttf";
 
 
 const WS_URL = 'ws://127.0.0.1:8000';
@@ -46,10 +49,10 @@ function App() {
     ));
   };
 
-  var experience = 0;
+  var [experience, setExperience] = useState('');
   const changeExperience = (num) => {
-    console.log(num)
     experience = num;
+    console.log("experience = " + experience)
     sendMessage(JSON.stringify({ 
       "experience" : experience,
       "type" : "public_c"} 
@@ -69,6 +72,12 @@ function App() {
     }
   }, [lastJsonMessage]);
 
+  var ellipse6left = "0px";
+  const setEllipse6left = (newPos) => {
+  ellipse6left = newPos;
+  console.log(ellipse6left)
+  };
+
   return (
     <>
       <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
@@ -82,12 +91,12 @@ function App() {
           <div className="rectangle" />
           <div className="text-wrapper">EXPERIENCE</div>
           <div className="experience-rec">
-              <button className="rectangle-2" onClick={e => {changeExperience(1)}}></button>;
-              <button className="rectangle-3" onClick={e => {changeExperience(2)}}></button>;
-              <button className="rectangle-4" onClick={e => {changeExperience(3)}}></button>;
-              <button className="rectangle-5" onClick={e => {changeExperience(4)}}></button>;
-              <button className="rectangle-6" onClick={e => {changeExperience(5)}}></button>;
-              <button className="rectangle-7" onClick={e => {changeExperience(6)}}></button>;
+              <button className="rectangle-2" onClick={e => {setExperience(1); changeExperience(1)}}></button>
+              <button className="rectangle-3" onClick={e => {setExperience(2); changeExperience(2)}}></button>
+              <button className="rectangle-4" onClick={e => {setExperience(3); changeExperience(3)}}></button>
+              <button className="rectangle-5" onClick={e => {setExperience(4); changeExperience(4)}}></button>
+              <button className="rectangle-6" onClick={e => {setExperience(5); changeExperience(5)}}></button>
+              <button className="rectangle-7" onClick={e => {setExperience(6); changeExperience(6)}}></button>
             
           </div>
           <img className="vector-tree" alt="Vector tree" src={require('./assets/Vector\ tree.png')} />
@@ -97,9 +106,17 @@ function App() {
           <div className="ellipse-2" />
           <div className="ellipse-3" />
           <div className="ellipse-4" />
+          <h1>{ellipse6left}</h1>
           <div className="ellipse-5" />
-          <img className="vector-2" alt="Vector" src="vector-2.svg" />
-          <div className="ellipse-6" />
+          <img className="vector-2" alt="Vector" src={require('./assets/Vector2.png')} />
+          {experience==1?  <img className="ellipse-6"/>: null}
+          {experience==2?  <img className="ellipse-7"/>: null}
+          {experience==3?  <img className="ellipse-8"/>: null}
+          {experience==4?  <img className="ellipse-9"/>: null}
+          {experience==5?  <img className="ellipse-10"/>: null}
+          {experience==6?  <img className="ellipse-11"/>: null}
+       
+          
 
     <div>
       <input value={slider1} type="range" onChange={e => {setSlider1(e.target.value); changeSlider1() }} ></input>
@@ -111,11 +128,55 @@ function App() {
           </div>
         </SwiperSlide>
 
-        <SwiperSlide>
-          <div class="swiper-slide slide2">
-            <h1>AUDIO</h1>
+  <SwiperSlide>
+    <div class="swiper-slide slide2">
+      
+      <div className="GALAXY2">
+        <div className="overlap-wrapper">
+          <div className="overlap">
+            <div className="rectangle" />
+              <div className="EMOTIONAL-STATE">
+              EMOTIONAL <br />
+              STATE
+              </div>
+              <img className="group" alt="Group" src={require('./assets/Group\ 2.png')} />
+              <div className="emotion">
+                <div className="overlap-group">
+                  <div className="div">
+                    <div className="overlap-2">
+                      <div className="group-2">
+                        <img className="arrow" alt="Arrow" src={require('./assets/Arrow\ 1.png')} />
+                        <img className="line" alt="Line" src={require('./assets/Line\ 2.png')} />
+                      </div>
+                      <div className="group-3">
+                        <img className="img" alt="Arrow" src={require('./assets/Arrow\ 2.png')} />
+                        <img className="line-2" alt="Line" src={require('./assets/Line\ 1.png')} />
+                      </div>
+                      <div className="text-wrapper">sad</div>
+                      <div className="text-wrapper-2">upset</div>
+                      <div className="text-wrapper-3">calm</div>
+                      <div className="text-wrapper-4">serene</div>
+                      <div className="text-wrapper-5">alert</div>
+                      <div className="text-wrapper-6">depressed</div>
+                      <div className="text-wrapper-7">excited</div>
+                      <div className="text-wrapper-8">fatigued</div>
+                      <div className="text-wrapper-9">happy</div>
+                      <div className="text-wrapper-10">valence+</div>
+                      <div className="text-wrapper-11">arousal+</div>
+                    </div>
+                    <div className="text-wrapper-12">nervous</div>
+                  </div>
+                  <div className="text-wrapper-13">stressed</div>
+                  <div className="text-wrapper-14">relaxed</div>
+                </div>
+              </div>
+            </div>
           </div>
-        </SwiperSlide>
+        </div>
+
+      </div>
+      
+  </SwiperSlide>
 
         <SwiperSlide>
           <div class="swiper-slide slide3">
